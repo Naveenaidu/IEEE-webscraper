a010535f37766ae2810c1ab3a75d8e0f6a1e8c56
Naveenaidu <naveennaidu479@gmail.com>
2018-02-09 13:19:34 +0530

    Defined all the Function. MAIN yet to be define

--- scibase-ieee-V1.py
+++ scibase-ieee-V1.py
@@ -10,6 +10,8 @@ from bs4 import BeautifulSoup
 from lxml import html
 from lxml.cssselect import CSSSelector
 
+citationCount = 0
+
 def get_response(url):
     try:
         browser.get(url)
@@ -144,3 +146,135 @@ def load_non_ieee_citations(browser):
                 continue
             except:
                 return
+################################################################
+#THE BELOW FUNCTIONS ARE FOR CITATIONS
+##Get the content of each citation paragraph
+def get_citation_tag(x,name,tree):
+    citations_tag = '#anchor-paper-citations-'+str(name)+' > div:nth-child(' + str(x) + ') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)'
+    citations = tree.cssselect(citations_tag)[0].text
+    return(citations_tag)
+
+##EXTRA CITATION SOUP OBJECT. GETS THE PP.,VOL, etc.
+#All the citations are under the class "description ng-binding". Here we are getting the soup list of all the citations.
+def extra_citat_info_tag(soup):#Pass the soup of the completely loaded page with cit.
+    citations_extra =  soup.find_all("div",{"class":"description ng-binding"})
+    return(citations_extra)
+
+##CITATION Authors
+def get_citation_authors(citation_tag):
+    try:
+        citations_authors = re.findall(r'([A-Za-z\s\.\-]+)\,\s',citation_tag,re.DOTALL)
+        error_catch = citations_authors[len(citations_authors)-1]#CAtching the error when no authors are present. CAN BE IMPROVED
+        a = '"authors":['
+        b = ""
+        for z in range(0,len(citations_authors)-1):
+            b = b + '"'+str(citations_authors[z])+'",'
+        #Printing the final author with appropriate tags
+        c = '"'+str(citations_authors[len(citations_authors)-1])+'"],'
+        authors = a+b+c
+        return(authors)
+
+    except:
+        authors = '"authors":["none"],'
+        return(authors)
+
+##CITATION ARTICLE NAME
+def get_citation_article_name(citation_tag):
+    try:
+        citations_article_name = re.findall(r'\"(.+)\"',citation_tag,re.DOTALL)
+        article_name = '"article name":"'+str(citations_article_name[0])+'",'
+        return(article_name)
+    except:
+        article_name = '"Article Name":"none",')
+        return(article_name)
+
+##JOURNAL NAME
+def get_citation_journal(x,name):
+    try:
+        citations_journal_cssselector = '#anchor-paper-citations-'+str(name) +'> div:nth-child(' + str(x) +') > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > em:nth-child(1)'
+        citations_journal_tag = tree.cssselect(citations_journal_cssselector)[0]
+        journal_name = '"Journal Name":"'+str(citations_journal_tag.text)+'",'
+        return(journal_name)
+    except:
+        journal_name = '"Journal Name":"none",'
+        return(journal_name)
+
+## CITATION VOLUME
+def get_citation_vol(citation):
+    try:
+        citations_vol = re.findall(r'vol\.\s([0-9A-Za-z\-]+)',citation,re.DOTALL)
+        citation_vol = '"vol.": "'+str(citations_vol[0])+'",'
+        return(citation_vol)
+    except:
+        citation_vol = '"vol":"none",'
+        return(citation_vol)
+
+## CITATION PP.
+def get_citation_pp(citation):
+    try:
+        citations_pp = re.findall(r'pp\.\s([A-Za-z0-9\-]+)',citation,re.DOTALL)
+        citation_pp = '"pp.": "'+str(citations_pp[0])+'",'
+        return(citation_pp)
+    except:
+        citation_pp = '"pp.":"none",'
+        return(citation_pp)
+
+## CITATION YEAR
+def get_citation_year(citation):
+    try:
+        citations_year = re.findall(r'\,\s([0-9]+)',citation,re.DOTALL)
+        citation_year = '"Year": "'+str(citations_year[0])+'",'
+        return(citation_year)
+    except:
+        citation_year = '"Year":"none",'
+        return(citation_year)
+
+##CITATION ISSN
+def get_citation_issn(citation):
+    try:
+        citations_issn = re.findall(r'ISSN\s([0-9\-A-Z]+)',citation,re.DOTALL)
+        citation_issn = '"ISSN": "'+str(citations_issn[0])+'",'
+        return(citation_issn)
+    except:
+        citation_issn= '"ISSN":"none",'
+        return(citation_issn)
+
+##CITATION ISBN
+def get_citation_isbn(citation):
+    try:
+        citations_isbn = re.findall(r'ISBN\s([0-9\-A-Z]+)',citation,re.DOTALL)
+        citation_isbn = '"ISBN": "'+str(citations_isbn[0])+'",'
+        return(citation_isbn)
+    except:
+        citation_isbn = '"ISBN":"none",'
+        return(citation_isbn)
+##################################################################
+def get_citation(num_citations,name,tree,soup):#soup of the completely loaded page
+#The citations are first scraped using bs4 using extra_citat_info_tag. Once we get the list of all the posibile values of citations from the soup object, we loop through them.
+#The get_citation_tag is used to get the particular division where the x'th citation is present using css selector
+    citation_json = '"{"'+str(name)+'-citations":[{'
+    citations_extra_info = extra_citat_info_tag(soup)
+    for x in range(2,num_citations+2):
+        citation = get_citation_tag(x,name,tree)
+        cit_author = get_citation_authors(citation)
+        cit_article_name = get_citation_article_name(citation)
+        cit_journal_name = get_citation_journal(citation)
+        #Getting the extra info of citations
+        cit_extra_info = citations_extra_info[citationCount++].text
+
+        cit_vol = get_citation_vol(cit_extra_info)
+        cit_pp = get_citation_pp(cit_extra_info)
+        cit_year = get_citation_year(cit_extra_info)
+        cit_issn = get_citation_issn(cit_extra_info)
+        cit_isbn = get_citation_isbn(cit_extra_info)
+
+        #Making the json of the citation
+        citation_json+= cit_author + cit_article_name + cit_journal_name + cit_vol + cit_pp + cit_year + cit_issn + cit_isbn
+
+        if( x <= num_citations): # x<= num_ieee_citations
+            citation_json+= "},"
+        else:
+            citation_json+= "}"
+
+    citation_json+= "]},"
+    return(citation_json)     
