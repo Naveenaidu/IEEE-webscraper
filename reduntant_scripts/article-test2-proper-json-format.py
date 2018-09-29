9633f290278d0ec0135b279b53c4a52e8f7a2b61
Naveenaidu <naveennaidu479@gmail.com>
2018-09-29 16:02:04 +0530

    Fixed Linting errors

--- reduntant_scripts/article-test2-proper-json-format.py
+++ reduntant_scripts/article-test2-proper-json-format.py
@@ -12,19 +12,17 @@ import json
 true = "true"
 false = "false"
 
-browser= webdriver.PhantomJS();
-article_link="http://ieeexplore.ieee.org/document/6546380/"
+browser = webdriver.PhantomJS()
+article_link = "http://ieeexplore.ieee.org/document/6546380/"
 
 
-
-
-#Get the page source
+# Get the page source
 browser.get(article_link)
-WebDriverWait(browser,20)
-wait = WebDriverWait(browser,12)
+WebDriverWait(browser, 20)
+wait = WebDriverWait(browser, 12)
 
 page_source = browser.page_source
-soup = BeautifulSoup(page_source,'html.parser')
+soup = BeautifulSoup(page_source, 'html.parser')
 soup_str = str(soup)
 
 '''
@@ -33,51 +31,57 @@ text = open("/home/theprophet/Atom/Scibase/article-page-source.txt")
 text = str(text.read())
 '''
 
-metadata = re.findall(r'global\.document\.metadata=(\{[\s\S]+\})\;',soup_str,re.DOTALL)
+metadata = re.findall(
+    r'global\.document\.metadata=(\{[\s\S]+\})\;', soup_str, re.DOTALL)
 json_data = json.dumps(json.loads(metadata[0]))
 print(json_data)
 
 
-#In order to avoid error where we have no issn
+# In order to avoid error where we have no issn
 try:
-    issn = re.findall(r'("issn":[\s\[\{\"\w\:\-\,\}]*])',json_data,re.DOTALL)
+    issn = re.findall(r'("issn":[\s\[\{\"\w\:\-\,\}]*])', json_data, re.DOTALL)
     print("{"+issn[0]+",")
 except:
     print('{"issn":"none",')
 
-#Adding ? to + in [\s\S]+? make the + which is a greedy operator lazy i.e it will try to match as few times as possible
-#[\s\S] means any space or non space character. It matches all characters
+# Adding ? to + in [\s\S]+? make the + which is a greedy operator lazy i.e it will try to match as few times as possible
+# [\s\S] means any space or non space character. It matches all characters
 try:
-    abstract = re.findall(r'(\s\"abstract\"\:[\s\S]+?\.\"\,)',json_data,re.DOTALL)
+    abstract = re.findall(
+        r'(\s\"abstract\"\:[\s\S]+?\.\"\,)', json_data, re.DOTALL)
     print(abstract[0])
 except:
-    abstract = re.findall(r'(\s\"abstract\"\:[\s\S]+?\"\,)',json_data,re.DOTALL)
+    abstract = re.findall(
+        r'(\s\"abstract\"\:[\s\S]+?\"\,)', json_data, re.DOTALL)
     print(abstract[0])
 
-#METRICS
-metrics = re.findall(r'("metrics":[\s\{\w\"\:\,]*})',json_data,re.DOTALL)
+# METRICS
+metrics = re.findall(r'("metrics":[\s\{\w\"\:\,]*})', json_data, re.DOTALL)
 print(metrics[0]+",")
 
-#DOI
-#Problem with the abstract for the link http://ieeexplore.ieee.org/document/6546378/
+# DOI
+# Problem with the abstract for the link http://ieeexplore.ieee.org/document/6546378/
 try:
-    doi = re.findall(r'(\"doi\"\:[\s\S]+?\"\,)',json_data,re.DOTALL)
+    doi = re.findall(r'(\"doi\"\:[\s\S]+?\"\,)', json_data, re.DOTALL)
     print(doi[0])
 except:
     print('"abstract":"none",')
 
-#TITLE
-title = re.findall(r'(\"title\"\:[\s\S]+?\"\,)',json_data,re.DOTALL)
+# TITLE
+title = re.findall(r'(\"title\"\:[\s\S]+?\"\,)', json_data, re.DOTALL)
 print(title[0])
 
-#publicationTitle
-publicationTitle = re.findall(r'(\"publicationTitle\"\:[\s\S]+?\"\,)',json_data,re.DOTALL)
+# publicationTitle
+publicationTitle = re.findall(
+    r'(\"publicationTitle\"\:[\s\S]+?\"\,)', json_data, re.DOTALL)
 print(publicationTitle[0])
 
-#We are getting the content between two keywords using regex. The () is the grouping element that gets displayed.
-t=re.findall(r'("authors":\s\[\{\"affiliation\"\:[\S\s]*)\,\s+\"isDynamicHtml\"\:',json_data,re.DOTALL)
+# We are getting the content between two keywords using regex. The () is the grouping element that gets displayed.
+t = re.findall(
+    r'("authors":\s\[\{\"affiliation\"\:[\S\s]*)\,\s+\"isDynamicHtml\"\:', json_data, re.DOTALL)
 if(t):
     print(t[0]+"}")
 else:
-    t = re.findall(r'("authors":\s\[\{\"affiliation\"\:[\S\s]*)\,\s+\"pubLink\"\:',json_data,re.DOTALL)
+    t = re.findall(
+        r'("authors":\s\[\{\"affiliation\"\:[\S\s]*)\,\s+\"pubLink\"\:', json_data, re.DOTALL)
     print(t[0]+"}")
